6a50131298626546a7f4b5dd0f6453e25cd48317
William Cui <37138564+spyrux@users.noreply.github.com>
2023-01-18 00:56:01 -0800

    Time: 0 ms (100.00%), Space: 42.6 MB (98.50%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,28 +1,33 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // Set the left and right boundaries
-        int left = 0, right = nums.length - 1;
         
-        // Under this condition
-        while (left <= right) {
-            // Get the middle index and the middle value.
-            int mid = left + (right - left) / 2;
+
+        
+        int high = nums.length -1;
+        int low = 0;
+        
+        while(low <= high){
             
-            // Case 1, return the middle index.
-            if (nums[mid] == target) {
+            int mid = high + (low-high)/2;
+            
+            
+            if(nums[mid] == target){
                 return mid;
-            } 
-            // Case 2, discard the smaller half.
-            else if (nums[mid] < target) {
-                left = mid + 1;   
-            } 
-            // Case 3, discard the larger half.
-            else {
-                right = mid - 1;
+
+            }else if(nums[mid] > target){
+                high = mid -1;
+                
+            }else if(nums[mid] < target){
+            
+               low = mid+1;
+                
             }
+            
         }
-        
-        // If we finish the search without finding target, return -1.
         return -1;
+        
+        
     }
+    
+  
 }
