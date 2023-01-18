8289fb6cfc4029e19a054c0ece8de7ee4f02bf73
William Cui <37138564+spyrux@users.noreply.github.com>
2023-01-18 00:01:17 -0800

    Time: 127 ms (25.22%), Space: 42.6 MB (59.46%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,61 +1,56 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] array = new int[26];
-        //default of 0 in new array
         
         if(s2.length() < s1.length()){
             return false;
         }
-        
+        int[] array1 = new int[26];
+        int[] array2 = new int[26];
+        //default of 0 in new array
         for(int i = 0; i < s1.length();i++){
-            array[s1.charAt(i)-'a']++;
-            array[s2.charAt(i)-'a']--;
+            array1[s1.charAt(i)-'a']++;
+            array2[s2.charAt(i)-'a']++;
         }
         
         int matches = 0;
         
-        for(int i = 0; i < array.length;i++){
-            if(array[i] == 0){
+        for(int i = 0; i < array1.length;i++){
+            if(array1[i]==array2[i]){
                 matches++;
             }
             
         }
-        
-        if(matches == 26){
-            return true;
-        }
+        int left = 0;
+        int right = s1.length() - 1;
         
         for(int i = s1.length(); i < s2.length(); i++){
-            
-                array[s2.charAt(i)-'a']--;
+                if (matches == 26) {
+                    return true;
+                }
+                array2[s2.charAt(i)-'a']++;
                 
-                if(array[s2.charAt(i)-'a'] == 0){
+                if(array2[s2.charAt(i)-'a'] == array1[s2.charAt(i)-'a']){
                     matches++;
-                }else if(array[s2.charAt(i)-'a'] == -1){
+                }else if(array2[s2.charAt(i)-'a']  - 1== array1[s2.charAt(i)-'a']){
                     matches--;
                 }
                 
                 int l = i - s1.length();
             
-                array[s2.charAt(l)-'a']++;
+                array2[s2.charAt(l)-'a']--;
             
-                if(array[s2.charAt(l)-'a'] == 0){
+                if(array2[s2.charAt(l)-'a'] == array1[s2.charAt(l)-'a']){
                     matches++;
-                }else if(array[s2.charAt(l)-'a'] == 1){
+                }else if(array2[s2.charAt(l)-'a'] + 1 == array1[s2.charAt(l)-'a']){
                     matches--;
                 }
-            
-                if (matches == 26) {
-                return true;
+                System.out.println(matches);
+                
             }
                 
               
                 
-            }
-        
-
             
-        
         
         
         
