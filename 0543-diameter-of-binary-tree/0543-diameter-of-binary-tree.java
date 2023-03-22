abfb6b1fefce28394389a9cce86475845bede0a3
William Cui <37138564+spyrux@users.noreply.github.com>
2023-03-21 18:22:30 -0700

    Time: 0 ms (100.00%), Space: 43 MB (12.39%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -17,24 +17,28 @@
 // at each parent node you add distance left and distance right and keep track of currmax also keep track of maxd of left and right 
 class Solution {
     
-    int maxDepth = 0;
+    
     
     public int diameterOfBinaryTree(TreeNode root) {
         
-        dfs(root);
-        return maxDepth;
+        int[] maxDepth = new int[1];
+        maxDepth[0] = 0; 
+        
+        int x = dfs(root,maxDepth);
+    
+        return maxDepth[0];
     }
     
-    public int dfs(TreeNode root){
+    public int dfs(TreeNode root, int[] maxDepth){
         
         if(root == null){
             return 0;
         }
         
-        int maxLeft = dfs(root.left);
-        int maxRight = dfs(root.right);
+        int maxLeft = dfs(root.left, maxDepth);
+        int maxRight = dfs(root.right, maxDepth);
         
-        maxDepth = Math.max(maxDepth, maxLeft + maxRight);
+        maxDepth[0] = Math.max(maxDepth[0], maxLeft + maxRight);
         
         return  Math.max(maxLeft, maxRight) + 1;
         
