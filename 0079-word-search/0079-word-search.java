089b95486df5e5dc3a4d893c2c5a5128084a1235
William Cui <37138564+spyrux@users.noreply.github.com>
2023-04-07 18:39:38 -0700

    Time: 170 ms (84.87%), Space: 41.1 MB (27.91%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,41 @@
+class Solution {
+    boolean exists = false;
+    
+    public boolean exist(char[][] board, String word) {
+        
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                int[][] seen = new int[board.length][board[0].length];
+                dfs(board, word, i, j, 0, seen);
+            }
+        }
+        
+        return exists;
+    }
+    
+    
+    
+    public void dfs(char[][] board, String word, int x, int y, int index, int[][] seen){
+        
+        if(x > board.length - 1|| y > board[0].length - 1||x < 0|| y < 0 || seen[x][y] == 1 || board[x][y]!= word.charAt(index)){
+            return;
+        }
+        
+        
+        if(index == word.length()-1){
+            exists = true;
+            return;
+        }
+           
+        seen[x][y] = 1;
+        dfs(board, word, x+1, y, index+1, seen);
+        dfs(board, word, x, y+1, index+1, seen);
+        dfs(board, word, x, y-1, index+1, seen);
+        dfs(board, word, x-1, y, index+1, seen);
+        seen[x][y] = 0;
+    }
+        
+       
+        
+        
+}
