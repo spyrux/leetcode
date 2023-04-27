a4bafba6a4cdd562d4fb29def7d247ba24fa1e72
William Cui <37138564+spyrux@users.noreply.github.com>
2023-04-26 20:15:13 -0700

    Time: 4 ms (70.70%), Space: 40.8 MB (57.35%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,59 +1,28 @@
 class Solution {
     
-    int amount = 0;
+   
     
     public int countSubstrings(String s) {
-        boolean[][] vis = new boolean[s.length()][s.length()];    
-        dfs(s, 0, 0, vis);
+        int count = 0;
+        for(int i = 0; i < s.length(); i++){
+            int odd = palindromes(s, i, i);
+            int even  = palindromes(s, i, i+1);
+            count += odd;
+            count += even;
+        }
+        
+        return count;
+        
+    }
+    
+    public int palindromes(String s, int start, int end){
+        int amount = 0;
+        while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)){
+            amount++;
+            start--;
+            end++;
+        }
         
         return amount;
     }
-    
-    public void dfs(String s, int start, int end, boolean[][] vis){
-        if(end > s.length() -1){
-            return;
-        }
-             
-        if(isPalindromic(s.substring(start,end +1))){
-            
-            if(!vis[start][end]){
-                amount++;
-            }
-            
-        }
-        vis[start][end] = true;
-        if(end < s.length() -1 && !vis[start][end+1] ){
-            dfs(s, start, end+1, vis);
-        }
-        if(end < s.length() -1 && !vis[start+1][end+1] ){
-            dfs(s, start+1, end+1, vis);
-        }
-        
-      
-    }    
-    
-    
-    
-    
-    
-    
-    
-    public boolean isPalindromic(String s){
-        
-        int low = 0;
-        int high = s.length()-1;
-        
-        while(low <= high){
-            if(s.charAt(low)!= s.charAt(high)){
-                return false;
-            }
-            
-            high--;
-            low++;
-        }
-        return true;
-    }
-    
-    
-        
 }
