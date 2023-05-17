583a7f94da15f72e66eb662cf159f6ca2dc593ce
William Cui <37138564+spyrux@users.noreply.github.com>
2023-05-16 23:27:57 -0700

    Time: 94 ms (5.57%), Space: 53.2 MB (30.85%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,48 +1,20 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         
-        int element = nums.length - k;
-        int high = nums.length - 1;
-        int low = 0;
+        List<Integer> list = new ArrayList();
+        for(int i = 0; i < nums.length; i++){
+            list.add(nums[i]);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(list);
         
+        int pop = pq.size() - k;
         
-        
-        return qs(nums, low, high, element);
-        
-        
-    }
-    
-    
-    
-    public void swap(int[] nums, int a, int b){
-        int c = nums[b];
-        nums[b] = nums[a];
-        nums[a] = c;
-        
-        
-    }
-    
-    public int qs(int[] nums, int l, int r, int element){
-        int p = l;
-        int pivot = r;
-        
-        for(int i = l; i < r; i++){
-            if(nums[i] <= nums[pivot]){
-                
-                swap(nums,p,i);
-                p++;
-                
-            }
+        while(pop > 0){
+            pq.poll();
+            pop--;
         }
         
-        swap(nums,pivot, p);
+        return pq.poll();
         
-        if(p == element){
-            return nums[p];
-        }else if(p > element){
-            return qs(nums, l, p-1, element);
-        }else{
-            return qs(nums, p+1, r, element);
-        }
     }
 }
