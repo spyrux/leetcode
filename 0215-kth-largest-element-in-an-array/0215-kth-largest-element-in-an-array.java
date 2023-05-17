253f5c4b9267ee3e8e72116dd7472a4a82e40cea
William Cui <37138564+spyrux@users.noreply.github.com>
2023-05-17 00:48:47 -0700

    Time: 34 ms (52.12%), Space: 50.4 MB (88.24%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,32 +1,42 @@
 class Solution {
     
     public int findKthLargest(int[] nums, int k) {
-	if (nums == null || nums.length == 0) return Integer.MAX_VALUE;
-    return findKthLargest(nums, 0, nums.length - 1, nums.length - k);
-}    
-
-public int findKthLargest(int[] nums, int start, int end, int k) {// quick select: kth smallest
-	if (start > end) return Integer.MAX_VALUE;
-	
-	int pivot = nums[end];// Take A[end] as the pivot, 
-	int left = start;
-	for (int i = start; i < end; i++) {
-		if (nums[i] <= pivot) // Put numbers < pivot to pivot's left
-			swap(nums, left++, i);			
-	}
-	swap(nums, left, end);// Finally, swap A[end] with A[left]
-	
-	if (left == k)// Found kth smallest number
-		return nums[left];
-	else if (left < k)// Check right part
-		return findKthLargest(nums, left + 1, end, k);
-	else // Check left part
-		return findKthLargest(nums, start, left - 1, k);
-} 
-
-void swap(int[] A, int i, int j) {
-	int tmp = A[i];
-	A[i] = A[j];
-	A[j] = tmp;				
-}
+        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
+    }
+    
+    public int quickSelect(int[] nums, int l, int r, int k) {
+        if(l > r){
+            return Integer.MAX_VALUE;
+        }
+        if (l == r) {
+            return nums[l];
+        }
+        
+        int p = r;
+        int pointer = l;
+        
+        for (int i = l; i < r; i++) {
+            if (nums[i] <= nums[p]) {
+                swap(nums, pointer, i);
+                pointer++;
+                
+            }
+        }
+       
+        swap(nums, pointer, p);
+        
+        if (pointer == k) {
+            return nums[pointer];
+        } else if (pointer > k) {
+            return quickSelect(nums, l, pointer - 1, k);
+        } else {
+            return quickSelect(nums, pointer + 1, r, k);
+        }
+    }
+    
+    public void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
 }
