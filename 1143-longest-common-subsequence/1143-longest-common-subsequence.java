257483e10e306226292b1bff64cbccd1cedec6f2
William Cui <37138564+spyrux@users.noreply.github.com>
2023-05-28 22:25:55 -0700

    Time: 30 ms (69.29%), Space: 48.5 MB (83.96%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,42 +3,24 @@ class Solution {
     
     public int longestCommonSubsequence(String text1, String text2) {
         
-        int[][] grid = new int[text1.length()][text2.length()];
+        int[][] grid = new int[text1.length()+1][text2.length()+1];
         
-        for(int i = 0; i < text1.length(); i++){
-            for(int j = 0; j < text2.length(); j++){
-                
-                
-                
-                
-                
-                if(text1.charAt(i) == text2.charAt(j)){
+        for(int i = 1; i < grid.length; i++){
+            for(int j = 1; j < grid[0].length; j++){
+
+               
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
                     
-                    if(j > 0 && i > 0){
-                        grid[i][j] = grid[i-1][j-1]+1;
-                    }else{
-                        grid[i][j] = 1;
-                    }
-                    
+                    grid[i][j] = Math.max(Math.max(grid[i-1][j],grid[i][j-1]),grid[i-1][j-1] + 1);
                     
                 }else{
-                    int max1 = 0;
-                    int max2 = 0;
-                    if(j > 0){
-                        max1 = grid[i][j-1];
-                    }
-                    if(i > 0){
-                        max2 = grid[i-1][j];
-                    }
-                    
-                    grid[i][j] = Math.max(max1, max2);
-                    
+                    grid[i][j] = Math.max(grid[i-1][j], grid[i][j-1]);      
                 }
                 
                
             }
         }
-        return grid[text1.length()-1][text2.length()-1];
+        return grid[text1.length()][text2.length()];
     }
     
     
