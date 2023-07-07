e81efb3153c00a3e5ea878d776db907e93cd202b
William Cui <37138564+spyrux@users.noreply.github.com>
2023-07-06 20:48:23 -0700

    Time: 66 ms (48.60%), Space: 17.2 MB (50.89%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        left = 0
+        right = len(s) -1 
+        a = s.lower()
+        
+        while(left < right):
+            
+            if (a[right].isalpha() or a[right].isnumeric()) and (a[left].isalpha() or a[left].isnumeric()):
+                if a[right] != a[left]:
+                    print(a[left])
+                    print(a[right])
+                    return False
+                else:
+                    right-=1
+                    left+=1
+            
+            
+            if not (a[left].isalpha() or a[left].isnumeric()):
+                left+=1
+            
+            if not (a[right].isalpha() or a[right].isnumeric()):
+                right-=1
+            
+            
+            
+        return True
+        
