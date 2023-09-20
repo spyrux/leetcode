71a4c3e6290a3e5eec3227478fc8cf05d01239b9
William Cui <37138564+spyrux@users.noreply.github.com>
2023-09-19 20:48:16 -0700

    Time: 63 ms (27.19%), Space: 16.6 MB (20.46%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -0,0 +1,72 @@
+
+class Solution:
+    
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        
+        q = []
+        visited = set()
+        total = 0
+        seen = 0
+        #find all indices of 2 and then dfs using those indices
+        minute = 0
+        self.m , self.n = len(grid), len(grid[0])
+        
+        rotten = []
+        
+        for (i, j) in product(range(self.m),range(self.n)):
+            if grid[i][j] == 1 or grid[i][j] == 2:
+                total+=1
+            if grid[i][j] == 2:
+                rotten.append((i,j))
+
+        for i in rotten:
+            q.append((0, i[0], i[1]))
+            
+            
+        while len(q) != 0:
+            
+            node = q.pop(0)
+            if (node[1],node[2]) in visited or grid[node[1]][node[2]] == 0:
+                continue
+        
+                
+            visited.add((node[1],node[2]))
+           
+            if grid[node[1]][node[2]] == 2 or grid[node[1]][node[2]] == 1:   
+                print(node[1],node[2])
+                grid[node[1]][node[2]] = 2  
+                minute = max(minute, node[0])
+            
+                
+                if node[1] > 0:
+                    q.append((minute+1, node[1]-1, node[2]))
+                if node[1] < self.m-1:
+                    q.append((minute+1, node[1]+1, node[2]))
+                if node[2] > 0:
+                    q.append((minute+1, node[1], node[2]-1))
+                if node[2] < self.n-1:
+                    q.append((minute+1, node[1], node[2]+1))
+              
+            
+  
+                
+                
+            
+        
+            
+
+                
+        
+        
+        seen = 0
+        for (i, j) in product(range(self.m),range(self.n)):
+            if grid[i][j] == 2:
+                seen+=1
+       
+        if total == seen:
+            return minute
+        
+        else: return -1
+                
+            
+        
