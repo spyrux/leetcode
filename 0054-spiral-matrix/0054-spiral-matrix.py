db10d72137d2e0e7b59351cfde7103f17e6cd4c1
William Cui <37138564+spyrux@users.noreply.github.com>
2024-04-03 18:41:25 -0700

    Time: 41 ms (18.37%), Space: 16.9 MB (5.09%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -16,22 +16,22 @@ class Solution:
         
         
         if dirCounter%4 == 0:
-            if j+1>= len(matrix[0]) or visited[i][j+1]:
+            if j+1>= len(matrix[0]) or visited[i][j+1]==1:
                 self.spiral(dirCounter+1, i+1, j, order, visited, matrix)
             else:
                 self.spiral(dirCounter, i, j+1, order, visited, matrix)
         elif dirCounter%4 == 1:
-            if i+1>= len(matrix) or visited[i+1][j]:
+            if i+1>= len(matrix) or visited[i+1][j]==1:
                 self.spiral(dirCounter+1, i, j-1, order, visited, matrix)
             else:
                 self.spiral(dirCounter, i+1, j, order, visited, matrix)
         elif dirCounter%4 == 2:
-            if j-1< 0 or visited[i][j-1]:
+            if j-1< 0 or visited[i][j-1]==1:
                 self.spiral(dirCounter+1, i-1, j, order, visited, matrix)
             else:
                 self.spiral(dirCounter, i, j-1, order, visited, matrix)
         else:
-            if i-1< 0 or visited[i-1][j]:
+            if i-1< 0 or visited[i-1][j]==1:
                 self.spiral(dirCounter+1, i, j+1, order, visited, matrix)
             else:
                 self.spiral(dirCounter, i-1, j, order, visited, matrix)
