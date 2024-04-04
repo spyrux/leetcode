0ed935db2b897f605e281a14c682679e9fc4ab8b
William Cui <37138564+spyrux@users.noreply.github.com>
2024-04-03 18:41:05 -0700

    Time: 32 ms (77.71%), Space: 16.6 MB (60.95%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,37 +1,37 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix or not matrix[0]:
-            return []
-
-        visited = [[0] * len(matrix[0]) for _ in matrix]
+        
+        visited = [[0 for i in matrix[0]] for i in matrix]
         order = []
-        self.spiral(0, 0, 0, order, visited, matrix)
+        dirCounter = 0
+        self.spiral(dirCounter, 0, 0, order, visited, matrix)
         return order
-
-    def spiral(self, dirCounter, i, j, order, visited, matrix):
+    
+    def spiral(self, dirCounter, i, j, order:List[int], visited:List[List[int]], matrix:List[List[int]]):
         if i < 0 or j < 0 or i >= len(matrix) or j >= len(matrix[0]) or visited[i][j]:
             return
 
         order.append(matrix[i][j])
         visited[i][j] = 1
-
-        if dirCounter % 4 == 0:  # Right
-            if j+1 >= len(matrix[0]) or visited[i][j+1]:
-                self.spiral(dirCounter+1, i+1, j, order, visited, matrix)  # Down
+        
+        
+        if dirCounter%4 == 0:
+            if j+1>= len(matrix[0]) or visited[i][j+1]:
+                self.spiral(dirCounter+1, i+1, j, order, visited, matrix)
             else:
                 self.spiral(dirCounter, i, j+1, order, visited, matrix)
-        elif dirCounter % 4 == 1:  # Down
-            if i+1 >= len(matrix) or visited[i+1][j]:
-                self.spiral(dirCounter+1, i, j-1, order, visited, matrix)  # Left
+        elif dirCounter%4 == 1:
+            if i+1>= len(matrix) or visited[i+1][j]:
+                self.spiral(dirCounter+1, i, j-1, order, visited, matrix)
             else:
                 self.spiral(dirCounter, i+1, j, order, visited, matrix)
-        elif dirCounter % 4 == 2:  # Left
-            if j-1 < 0 or visited[i][j-1]:
-                self.spiral(dirCounter+1, i-1, j, order, visited, matrix)  # Up
+        elif dirCounter%4 == 2:
+            if j-1< 0 or visited[i][j-1]:
+                self.spiral(dirCounter+1, i-1, j, order, visited, matrix)
             else:
                 self.spiral(dirCounter, i, j-1, order, visited, matrix)
-        else:  # Up
-            if i-1 < 0 or visited[i-1][j]:
-                self.spiral(dirCounter+1, i, j+1, order, visited, matrix)  # Right
+        else:
+            if i-1< 0 or visited[i-1][j]:
+                self.spiral(dirCounter+1, i, j+1, order, visited, matrix)
             else:
                 self.spiral(dirCounter, i-1, j, order, visited, matrix)
